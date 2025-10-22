d8b94fcc45e4b60cc21e177d70ef0175a0e844ae
Hernan Alvarado <hernanvid123@gmail.com>
2025-10-21 20:09:06 -0500

    docs: add `schemas.mdx` file

--- docs/src/lib/metadata.ts
+++ docs/src/lib/metadata.ts
@@ -1,6 +1,6 @@
 import { Metadata } from "next"
 
-const title = "@aura-stack/router"
+const title = "AuraStack Router"
 const description = "It is a modern, TypeScript-first router and endpoint definition library for TypeScript backend applications."
 
 const url = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://aura-stack.dev"
