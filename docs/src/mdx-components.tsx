7ca8a4dc6f3886806914d16a20667f8c6f356130
Hernan Alvarado <hernanvid123@gmail.com>
2025-11-18 14:59:19 -0500

    docs: update docs
    - Update `create-endpoint-config` API Reference
    - Update `zod-schemas` docs
    - Update `middlewares` docs
    - Add `on-error` docs

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from "fumadocs-ui/mdx"
 import type { MDXComponents } from "mdx/types"
 import * as Twoslash from "fumadocs-twoslash/ui"
+import * as StepComponents from "fumadocs-ui/components/steps"
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
     return {
         ...defaultMdxComponents,
         ...Twoslash,
+        ...StepComponents,
         ...components,
     }
 }
